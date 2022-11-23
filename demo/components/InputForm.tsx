5e2214f696f13caf65092c2ca4a6da58e0f9cf53
Tim Evci <tevci93@gmail.com>
2022-11-23 15:38:39 +0300

    Added form validation.

--- demo/components/InputForm.tsx
+++ demo/components/InputForm.tsx
@@ -1,20 +1,23 @@
 import { Button, CardActions } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import { useEffect, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { IFormInput } from "../interfaces/IFormInput";
 import { TextFieldController } from "./TextFieldController";
-import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
 
 export interface InputFormProps {
     commonCardVisible: boolean;
     onSubmit: SubmitHandler<IFormInput>;
     code: string;
-    changeCardVisible(): void
+    changeCardVisible(): void;
 }
-const InputForm = ({ commonCardVisible, onSubmit, code, changeCardVisible }: InputFormProps) => {
-
-    
+const InputForm = ({
+    commonCardVisible,
+    onSubmit,
+    code,
+    changeCardVisible,
+}: InputFormProps) => {
     // Move useForm here
     // use the watch("code") to disable send button when the code hasn't changed
     // You can track this with a simple code, setCode useState()
@@ -26,18 +29,29 @@ const InputForm = ({ commonCardVisible, onSubmit, code, changeCardVisible }: Inp
         code: "```mermaid \n classDiagram\r\n    Animal <|-- Duck\r\n    Animal <|-- Fish\r\n    Animal <|-- Zebra\r\n    Singleton --> Singleton\r\n    Animal : +int age\r\n    Animal : +String gender\r\n    Animal: +isMammal()\r\n    Animal: +mate()\r\n    class Duck{\r\n        +String beakColor\r\n        +swim()\r\n        +quack()\r\n    }\r\n    class Fish{\r\n        -int sizeInFeet\r\n        -canEat()\r\n    }\r\n    class Zebra{\r\n        +bool is_wild\r\n        +run()\r\n    }\r\n    class Singleton{\r\n      -Singleton singleton$\r\n      -Singleton()\r\n      +getInstance()$ Singleton    \r\n    }\n```",
     };
 
-    const { handleSubmit, control, watch, reset } = useForm<IFormInput>({
+    const { handleSubmit, control, reset, watch } = useForm<IFormInput>({
         defaultValues,
+        mode: "onChange",
     });
 
     function resetCode() {
         reset({
             code: defaultValues.code || "",
         });
+        setSubmitButtonDisabled(false);
     }
 
+    const onHandleSubmit = async (data: IFormInput) => {
+        onSubmit(data);
+        setSubmitButtonDisabled(true);
+    };
+
+    useEffect(() => {
+        setSubmitButtonDisabled(false);
+    }, [watch("code")]);
+
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onHandleSubmit)}>
             <Card
                 sx={{
                     minWidth: 200,
@@ -68,7 +82,10 @@ const InputForm = ({ commonCardVisible, onSubmit, code, changeCardVisible }: Inp
                         Send
                     </Button>
                     <Button
-                        onClick={() => {resetCode; changeCardVisible()}}
+                        onClick={() => {
+                            resetCode();
+                            changeCardVisible();
+                        }}
                         variant="text"
                         data-cy="reset_Button"
                         sx={{ ml: "10px" }}
